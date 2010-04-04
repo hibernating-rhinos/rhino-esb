033b3edc914f85413c2c0cff958099d91ede090e
Ayende Rahien <Ayende@ayende.com>
2010-04-04 11:59:49 +0300

    Moving to common message handling completion

--- Rhino.ServiceBus/RhinoQueues/RhinoQueuesTransport.cs
+++ Rhino.ServiceBus/RhinoQueues/RhinoQueuesTransport.cs
@@ -319,79 +319,10 @@ namespace Rhino.ServiceBus.RhinoQueues
 			}
 			finally
 			{
-				HandleMessageCompletion(message, tx, ex, messageCompleted, beforeTransactionCommit);
+				var messageHandlingCompletion = new MessageHandlingCompletion(tx, null, ex, messageCompleted, beforeTransactionCommit, logger,
+				                                                              MessageProcessingFailure, currentMessageInformation);
+				messageHandlingCompletion.HandleMessageCompletion();
 				currentMessageInformation = null;
-			}
-		}
-
-		private void HandleMessageCompletion(Message message, TransactionScope tx, Exception exception, Action<CurrentMessageInformation, Exception> messageCompleted, Action<CurrentMessageInformation> beforeTransactionCommit)
-		{
-			var txDisposed = false;
-			if (exception == null)
-			{
-				try
-				{
-					if (tx != null)
-					{
-						if(beforeTransactionCommit!=null)
-							beforeTransactionCommit(currentMessageInformation);
-						tx.Complete();
-						tx.Dispose();
-						txDisposed = true;
-					}
-					try
-					{
-						if (messageCompleted != null)
-							messageCompleted(currentMessageInformation, exception);
-					}
-					catch (Exception e)
-					{
-						logger.Error("An error occured when raising the MessageCompleted event, the error will NOT affect the message processing", e);
-					}
-					return;
-				}
-				catch (Exception e)
-				{
-					logger.Warn("Failed to complete transaction, moving to error mode", e);
-					exception = e;
-				}
-			}
-			try
-			{
-				if (txDisposed == false && tx != null)
-				{
-					logger.Warn("Disposing transaction in error mode");
-					tx.Dispose();
-				}
-			}
-			catch (Exception e)
-			{
-				logger.Warn("Failed to dispose of transaction in error mode.", e);
-			}
-			if (message == null)
-				return;
-			try
-			{
-				if (messageCompleted != null)
-					messageCompleted(currentMessageInformation, exception);
-			}
-			catch (Exception e)
-			{
-				logger.Error("An error occured when raising the MessageCompleted event, the error will NOT affect the message processing", e);
-			}
-			try
-			{
-				var copy = MessageProcessingFailure;
-				if (copy != null)
-					copy(currentMessageInformation, exception);
-			}
-			catch (Exception moduleException)
-			{
-				string exMsg = "";
-				if (exception != null)
-					exMsg = exception.Message;
-				logger.Error("Module failed to process message failure: " + exMsg,
-											 moduleException);
 			}
 		}
 
