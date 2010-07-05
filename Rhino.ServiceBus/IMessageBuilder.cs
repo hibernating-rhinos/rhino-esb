061fac320bb197946f22671a56904eb093ca10c4
mnichols <ceiapps@gmail.com>
2010-07-05 11:23:53 -0700

    introduced imessage and moved imessagebuilder

---
+++ Rhino.ServiceBus/IMessageBuilder.cs
@@ -0,0 +1,9 @@
+﻿using System.Messaging;
+
+namespace Rhino.ServiceBus.Msmq
+{
+    public interface IMessageBuilder
+    {
+        Message GenerateMsmqMessageFromMessageBatch(params object[] msgs);
+    }
+}
