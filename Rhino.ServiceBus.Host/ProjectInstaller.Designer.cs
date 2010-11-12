5cb16bb7708085a35408684e4971df57c7ce95ca
Corey Kaylor <corey@kaylors.net>
2010-11-12 14:25:56 -0700

    Removed MSMQ dependency from the host installer for those that choose to use rhino-queues for a transport.

--- Rhino.ServiceBus.Host/ProjectInstaller.Designer.cs
+++ Rhino.ServiceBus.Host/ProjectInstaller.Designer.cs
@@ -28,25 +28,23 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
-			this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
-			// 
-			// serviceProcessInstaller1
-			// 
-			this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.NetworkService;
-			this.serviceProcessInstaller1.Password = null;
-			this.serviceProcessInstaller1.Username = null;
-			// 
-			// serviceInstaller1
-			// 
-			this.serviceInstaller1.ServiceName = "RhinoServiceBusHost";
-			this.serviceInstaller1.ServicesDependedOn = new string[] {
-        "MSMQ"};
-			this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
-			// 
-			// ProjectInstaller
-			// 
-			this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.NetworkService;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.ServiceName = "RhinoServiceBusHost";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
             this.serviceProcessInstaller1,
             this.serviceInstaller1});
 
