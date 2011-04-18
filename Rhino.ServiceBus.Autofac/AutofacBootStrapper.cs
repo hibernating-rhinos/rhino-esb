5837f6d4c6c2ca9089e821deb30d29e14e351079
Steve Wagner <lanwin@lanwin.de>
2011-04-18 20:25:03 +0200

    Impl. AutofacBootStrapper.

--- Rhino.ServiceBus.Autofac/AutofacBootStrapper.cs
+++ Rhino.ServiceBus.Autofac/AutofacBootStrapper.cs
@@ -1,11 +1,18 @@
+using System.Collections.Generic;
+using System.Reflection;
 using Autofac;
+using Autofac.Core;
+using Autofac.Core.Registration;
+using Rhino.ServiceBus.Actions;
 using Rhino.ServiceBus.Hosting;
+using Rhino.ServiceBus.Impl;
+using Rhino.ServiceBus.Internal;
 
 namespace Rhino.ServiceBus.Autofac
 {
     public class AutofacBootStrapper : AbstractBootStrapper
     {
-        private readonly IContainer container;
+        private IContainer container;
 
         public AutofacBootStrapper()
         {
@@ -21,29 +28,77 @@ namespace Rhino.ServiceBus.Autofac
             get { return container; }
         }
 
-        public override void CreateContainer()
+        protected override void ConfigureBusFacility(AbstractRhinoServiceBusConfiguration configuration)
         {
-        }
-
-        protected virtual void ConfigureContainer()
-        {
-            
+            configuration.UseAutofac(container);
         }
 
         public override void ExecuteDeploymentActions(string user)
         {
+            foreach(var action in container.Resolve<IEnumerable<IDeploymentAction>>())
+            {
+                action.Execute(user);
+            }
         }
 
         public override void ExecuteEnvironmentValidationActions()
         {
+            foreach(var action in container.Resolve<IEnumerable<IEnvironmentValidationAction>>())
+            {
+                action.Execute();
+            }
         }
 
         public override T GetInstance<T>()
         {
-            return default(T);
+            return container.Resolve<T>();
         }
 
         public override void Dispose()
+        {
+            container.Dispose();
+        }
+
+        public override void CreateContainer()
+        {
+            if(container == null)
+                container = new ContainerBuilder().Build();
+
+            ConfigureContainer();
+        }
+
+        protected virtual void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(Assembly)
+                .AssignableTo<IDeploymentAction>()
+                .SingleInstance();
+            builder.RegisterAssemblyTypes(Assembly)
+                .AssignableTo<IEnvironmentValidationAction>()
+                .SingleInstance();
+            
+            builder.Update(container);
+            
+            RegisterConsumersFrom(Assembly);
+        }
+
+        protected virtual void RegisterConsumersFrom(Assembly assembly)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(assembly)
+                .Where(type =>
+                    typeof(IMessageConsumer).IsAssignableFrom(type) &&
+                        typeof(IOccasionalMessageConsumer).IsAssignableFrom(type) == false &&
+                            IsTypeAcceptableForThisBootStrapper(type))
+                .OnRegistered(e => ConfigureConsumer(e.ComponentRegistration))
+                .InstancePerDependency();
+
+            builder.Update(container);
+        }
+
+        protected virtual void ConfigureConsumer(IComponentRegistration registration)
         {
         }
     }
