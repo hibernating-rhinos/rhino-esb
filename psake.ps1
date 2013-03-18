33d9b1fcc07ad70d1275fd81f7c1d8ca668323f1
Valeriu Caraulean <caraulean@gmail.com>
2013-03-18 17:47:13 +0100

    Updated psake to latest version - 4.2
    Just copied required files from psake's master branch.
    Old scripts were failing on Windows8 x64 with an error that was fixed in
    psake some time ago, now it's building fine...

--- psake.ps1
+++ psake.ps1
@@ -5,25 +5,47 @@
 # Must match parameter definitions for psake.psm1/invoke-psake 
 # otherwise named parameter binding fails
 param(
-  [Parameter(Position=0,Mandatory=0)]
-  [string]$buildFile = 'default.ps1',
-  [Parameter(Position=1,Mandatory=0)]
-  [string[]]$taskList = @(),
-  [Parameter(Position=2,Mandatory=0)]
-  [string]$framework = '3.5',
-  [Parameter(Position=3,Mandatory=0)]
-  [switch]$docs = $false,
-  [Parameter(Position=4,Mandatory=0)]
-  [System.Collections.Hashtable]$parameters = @{},
-  [Parameter(Position=5, Mandatory=0)]
-  [System.Collections.Hashtable]$properties = @{}
+    [Parameter(Position=0,Mandatory=0)]
+    [string]$buildFile = 'default.ps1',
+    [Parameter(Position=1,Mandatory=0)]
+    [string[]]$taskList = @(),
+    [Parameter(Position=2,Mandatory=0)]
+    [string]$framework,
+    [Parameter(Position=3,Mandatory=0)]
+    [switch]$docs = $false,
+    [Parameter(Position=4,Mandatory=0)]
+    [System.Collections.Hashtable]$parameters = @{},
+    [Parameter(Position=5, Mandatory=0)]
+    [System.Collections.Hashtable]$properties = @{},
+    [Parameter(Position=6, Mandatory=0)]
+    [alias("init")]
+    [scriptblock]$initialization = {},
+    [Parameter(Position=7, Mandatory=0)]
+    [switch]$nologo = $false,
+    [Parameter(Position=8, Mandatory=0)]
+    [switch]$help = $false,
+    [Parameter(Position=9, Mandatory=0)]
+    [string]$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.path)
 )
 
-remove-module psake -ea 'SilentlyContinue'
-$scriptPath = Split-Path -parent $MyInvocation.MyCommand.path
+$currentThread = [System.Threading.Thread]::CurrentThread
+$invariantCulture = [System.Globalization.CultureInfo]::InvariantCulture
+$currentThread.CurrentCulture = $invariantCulture
+$currentThread.CurrentUICulture = $invariantCulture
+
+# '[p]sake' is the same as 'psake' but $Error is not polluted
+remove-module [p]sake
 import-module (join-path $scriptPath psake.psm1)
-if (-not(test-path $buildFile))
-{
-    $buildFile = (join-path $scriptPath $buildFile)
+if ($help) {
+  Get-Help Invoke-psake -full
+  return
+}
+
+if (-not(test-path $buildFile)) {
+    $absoluteBuildFile = (join-path $scriptPath $buildFile)
+    if (test-path $absoluteBuildFile) {
+        $buildFile = $absoluteBuildFile
+    }
 } 
-invoke-psake $buildFile $taskList $framework $docs $parameters $properties
+
+invoke-psake $buildFile $taskList $framework $docs $parameters $properties $initialization $nologo
